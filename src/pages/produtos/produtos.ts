aa26e7290940c04428f08081c88ac5e64f47dee0
Vinicius da Silva Cruz <vinicius.cruz@CONTMATIC.AD>
2020-09-01 00:15:31 -0300

    carregando a imagem dos produtos

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,3 +1,4 @@
+import { API_CONFIG } from './../../config/api.config';
 import { ProdutoService } from './../../services/domain/produto.service';
 import { ProdutoDTO } from './../../models/produto.dto';
 import { Component } from '@angular/core';
@@ -14,14 +15,26 @@ export class ProdutosPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public produto_service: ProdutoService) {
   }
-  
+
 
   ionViewDidLoad() {
     let categoria_id = this.navParams.get('categoriaId');
     this.produto_service.findByCategoria(categoria_id).subscribe(response => {
       this.items = response['content'];
+      this.loadImageUrls();
     },
-    erro => {});
+      erro => { });
   }
 
+  loadImageUrls() {
+    for (var i = 0; i < this.items.length; i++) {
+      let item = this.items[i];
+      this.produto_service.getSmallImageFromBucket(item.id)
+        .subscribe(response => {
+          item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
+        },
+          error => { });
+    }
+  }
 }
+
--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs/Rx';
 import { API_CONFIG } from './../../config/api.config';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from "@angular/core";
@@ -11,5 +12,10 @@ export class ProdutoService {
 
     findByCategoria(categoria_id: string) {
         return this.http.get(`${API_CONFIG.baseUrl}/produtos/?nome&categoria=${categoria_id}`);
+    }
+
+    getSmallImageFromBucket(id: string): Observable<any> {
+        let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`;
+        return this.http.get(url, {responseType: 'blob'});
     }
 }
