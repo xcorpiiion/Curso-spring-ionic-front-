84d140f7d070982300e0ced60fba1714b5605d79
Vinicius da Silva Cruz <vinicius.cruz@CONTMATIC.AD>
2020-08-31 22:38:12 -0300

    Criando pagina de produtos

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -33,6 +33,10 @@ export class CategoriasPage {
       this.items = response;
     },
       error => {
-      })
+      });
+  }
+
+  showProdutos() {
+    this.navCtrl.push('ProdutosPage');
   }
 }
