f109ea406369f75ffe90a8820ef16edb8014ef88
Vinicius da Silva Cruz <vinicius.cruz@CONTMATIC.AD>
2020-09-01 22:31:45 -0300

    Carregamento de dados do produto

--- src/pages/produto-detail/produto-detail.ts
+++ src/pages/produto-detail/produto-detail.ts
@@ -1,3 +1,5 @@
+import { API_CONFIG } from './../../config/api.config';
+import { ProdutoService } from './../../services/domain/produto.service';
 import { ProdutoDTO } from './../../models/produto.dto';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
@@ -11,15 +13,22 @@ export class ProdutoDetailPage {
 
   item: ProdutoDTO;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public produto_service: ProdutoService) {
   }
 
   ionViewDidLoad() {
-    this.item = {
-      id: "1",
-      nome: "Mouse",
-      preco: 60.00
-    };
+    let produto_id = this.navParams.get('produtoId');
+    this.produto_service.findBYId(produto_id).subscribe(response => {
+      this.item = response;
+    },
+    error => {});
+  }
+
+  getImageUrlIfExists() {
+    this.produto_service.getImageFromBucket(this.item.id).subscribe(response => {
+      this.item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${this.item.id}.jpg`;
+    }),
+    error => {}; 
   }
 
 }
--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -1,3 +1,4 @@
+import { ProdutoDTO } from './../../models/produto.dto';
 import { Observable } from 'rxjs/Rx';
 import { API_CONFIG } from './../../config/api.config';
 import { HttpClient } from '@angular/common/http';
@@ -10,12 +11,21 @@ export class ProdutoService {
 
     }
 
+    findBYId(produto_id: string) {
+        return this.http.get<ProdutoDTO>(`${API_CONFIG.baseUrl}/produtos/${produto_id}`);
+    }
+
     findByCategoria(categoria_id: string) {
         return this.http.get(`${API_CONFIG.baseUrl}/produtos/?nome&categoria=${categoria_id}`);
     }
 
     getSmallImageFromBucket(id: string): Observable<any> {
         let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`;
-        return this.http.get(url, {responseType: 'blob'});
+        return this.http.get(url, { responseType: 'blob' });
+    }
+
+    getImageFromBucket(id: string): Observable<any> {
+        let url = `${API_CONFIG.bucketBaseUrl}/prod${id}.jpg`;
+        return this.http.get(url, { responseType: 'blob' });
     }
 }
